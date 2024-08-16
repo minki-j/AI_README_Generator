8d8d99f296df040a38a62368beff468d05bc680d
minki-j <qmsoqm2@gmail.com>
2024-08-16 15:43:30 -0400

    Frontend: add user feedback loop

--- backend/app/api/routes/middle_steps.py
+++ backend/app/api/routes/middle_steps.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi import Body
 
 from app.langgraph.main_graph import main_graph
 from ..common import THREAD
@@ -10,9 +11,15 @@ router = APIRouter()
 def root():
     return {"message": "/middleSteps route working fine"}
 
+
 @router.post("")
-def middle_steps():
+def middle_steps(user_feedback: str = Body(...), llm_output: str = Body(...)):
     print("--- /middleSteps POST ---")
+    print(f"==>> user_feedback: {user_feedback}")
+    print(f"==>> llm_output: {llm_output}")
     res = main_graph.invoke(None, THREAD)
     print(f"==>> res: {res}")
-    return {"message": "middleSteps route working fine"}
+    return {
+        "next_step": "Step 2: Analyzing the repository",
+        "llm_output": "Analyzing the repository...",
+    }
--- frontend/app/home/main.py
+++ frontend/app/home/main.py
@@ -80,6 +80,32 @@ def get():
     )
 
 
+def return_confirmation_box(next_step, llm_output):
+    return Div(
+        Div(
+            Titled("Let's write a README together!"),
+            P(next_step),
+            Form(
+                Div(
+                    Input(name="llm_output", value=llm_output),
+                    Input(name="user_feedback", placeholder="Enter your feedback here"),
+                    Group(
+                        Button("Send", type="submit", cls="outline col-xs-6"),
+                        Button("Retry", cls="outline col-xs-6", hx_get="/retry"),
+                        cls="row between-xs",
+                    ),
+                    cls="row around-xs",
+                ),
+                hx_post="/middle_steps",
+                hx_swap="outerHTML",
+                target_id="middle_step",
+            ),
+            cls="container bordered-container",
+        ),
+        id="middle_step",
+    )
+
+
 @rt("/start")
 def post(clone_url: str):
     # Check if the URL is a valid GitHub clone URL
@@ -90,11 +116,18 @@ def post(clone_url: str):
             cls="error-message",
         )
 
-    url = os.getenv("BACKEND_BASE_URL_DEV") + "/start"
+    url = os.getenv("BACKEND_BASE_URL") + "/start"
     print("Request url: ", url)
 
     r = requests.post(url, json={"clone_url": clone_url})
     print(f"==>> r: {r}")
+    next_step = "Step 2: Analyzing the repository"
+    llm_output = "Analyzing the repository..."
+
+    if r.status_code != 200:
+        confirmation_box = P("Error: Something went wrong. Please try again later.")
+    else:
+        confirmation_box = return_confirmation_box(next_step, llm_output)
 
     return (
         Input(
@@ -103,36 +136,50 @@ def post(clone_url: str):
             hx_swap_oob="true",
             id="clone_url_input",
         ),
-        (
-            Div(
-                Div(
-                    Titled("Let's write a README together!"),
-                    P("Press Start to start cloning your repository."),
-                    Button("Start", type="submit", cls="outline"),
-                    cls="container bordered-container",
-                ),
-                hx_post="/middle_steps",
-                hx_swap="outerHTML",
-                target_id="middle_step",
-                id="middle_step",
-            )
-            if r.status_code == 200
-            else P("Error: Unable to clone the repository.")
-        ),
+        confirmation_box,
     )
 
 
 @rt("/middle_steps")
-def post():
+def post(
+    llm_output: str,
+    user_feedback: str,
+):
+    print("user_feedback: ", user_feedback)
+    url = os.getenv("BACKEND_BASE_URL") + "/middleSteps"
+    print("Request url: ", url)
+
+    r = requests.post(
+        url, json={"user_feedback": user_feedback, "llm_output": llm_output}
+    )
+    print(f"==>> r: {r}")
+    next_step = "Step 2: Analyzing the repository"
+    llm_output = "Analyzing the repository..."
+
+    return return_confirmation_box(next_step, llm_output)
+
+
+@rt("/retry")
+def get():
+    url = os.getenv("BACKEND_BASE_URL") + "/retry"
+    r = requests.get(url)
+
     return Div(
         Div(
             Titled("Let's write a README together!"),
             P("Step 1: Cloning the repository"),
             Form(
                 Div(
-                    Button("Start", type="submit", cls="outline col-xs-6"),
-                    Button("Cancel", type="submit", cls="outline secondary col-xs-6"),
-                    cls="row",
+                    Input(name="user_feedback", placeholder="Enter your feedback here"),
+                    Group(
+                        Button("Send", type="submit", cls="outline col-xs-3"),
+                        Button("Retry", cls="outline col-xs-3", hx_get="/retry"),
+                        Button(
+                            "Go Back", type="submit", cls="outline secondary col-xs-3"
+                        ),
+                        cls="row around-xs",
+                    ),
+                    cls="row around-xs",
                 ),
                 hx_post="/middle_steps",
                 hx_swap="outerHTML",
